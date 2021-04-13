0b43f8269f5bc8dfdde3c6ec57da9da3d5947dd3
yesui12 <yesui1209@gmail.com>
2021-04-13 21:26:06 +0800

    Huuhaslalt bolon Search model implement hiine

--- src/js/view/base.js
+++ src/js/view/base.js
@@ -1,8 +1,9 @@
 export const elements = {
     searchForm: document.querySelector(".search"),
-    searchInput : document.querySelector(".search__field"),
-    searchResultDiv : document.querySelector(".results"),
-    searchResultList : document.querySelector(".results__list")
+    searchInput: document.querySelector(".search__field"),
+    searchResultDiv: document.querySelector(".results"),
+    searchResultList: document.querySelector(".results__list"),
+    pageButtons: document.querySelector(".results__pages")
 };
 
 export const elementStrings = {
